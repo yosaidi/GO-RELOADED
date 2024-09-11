33f788f62154558b793374e45209190ec54f9abc
yosaidi <younes.saidi.23@ump.ac.ma>
2024-09-11 12:39:17 +0100

    go-reloaded

--- modifications/case.go
+++ modifications/case.go
@@ -10,9 +10,8 @@ func Case(s *[]string, prefix string, f func(string) string) {
 	for i := 0; i < len(slice); i++ {
 		if strings.Contains(slice[i], prefix) {
 			if strings.Contains(slice[i], prefix+",") {
-				number, err := strconv.Atoi(slice[i+1][:len(slice[i+1])-1])
+				number, err := strconv.Atoi(slice[i+1][:FindParenthese(slice[i+1])])
 				if err != nil || number > len(slice) || number < 0 {
-
 					continue
 				}
 				for j := i - number; j < i; j++ {
@@ -21,12 +20,13 @@ func Case(s *[]string, prefix string, f func(string) string) {
 						slice[j] = f(slice[j])
 					}
 				}
+				slice[i-1] += slice[i+1][FindParenthese(slice[i+1])+1:]
 				slice = append(slice[:i], slice[i+2:]...)
 			} else {
 				if i == 0 {
 					slice = slice[1:]
 				} else {
-					slice[i-1]+= slice[i][len(prefix)+1:]
+					slice[i-1] += slice[i][len(prefix)+1:]
 					slice[i-1] = f(slice[i-1])
 					slice = append(slice[:i], slice[i+1:]...)
 				}
@@ -34,4 +34,15 @@ func Case(s *[]string, prefix string, f func(string) string) {
 		}
 	}
 	*s = slice
+}
+
+func FindParenthese(s string) int {
+	count := 0
+
+	for i, ch := range s {
+		if ch == ')' {
+			count = i
+		}
+	}
+	return count
 }
